b5f3f35d161a99d0ac8f91ff7b4ef3a4655b5df1
geodimitrov <geo.dimitrov@yahoo.com>
2021-05-20 22:00:49 +0300

    Added Exercises folder & solutions

---
+++ Data-Types-&-Variables/Exercises/05. booleanVariable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string booleanString;
+    cin >> booleanString;
+
+    if (booleanString == "True"){
+        cout << "Yes";
+    }
+
+    else {
+        cout << "No";
+    }
+
+}
---
+++ Data-Types-&-Variables/Exercises/09. reversedChars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string letterOne, letterTwo, letterThree;
+    cin >> letterOne;
+    cin >> letterTwo;
+    cin >> letterThree;
+    cout << letterThree + letterTwo + letterOne;
+
+}
